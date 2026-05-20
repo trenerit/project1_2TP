d41ec437927996e8281d7b196d7e16bb44f34cb4
Piotr Kania <piotr@trener-it.pl>
2026-05-20 15:33:12 +0200

    style: update header button functionality and restore script inclusion in HTML

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 'use strict';
+console.log('start');
+// const
+// let
+// var
+
+// for(let i = 1; i <= 10; i++) {
+    //     console.log(i);
+    // }
+    
+    // console.log('To jest poza pętlą',i);
+    
+    // const tab1 = [];
+    
+    // tab1.push(100);
+    
+    // console.log(tab1);
+    
+    function kania() {
+        console.log('jsdhfjhsgjsah');
+    }
+    
+    const btn1 = document.getElementById('headerBtn');
+    
+    console.log(btn1);
+    
+    btn1.addEventListener('click', () => {
+        console.log('jsdhfjhsgjsah');
+        const h1 = document.querySelector('header h1');
+        h1.innerText = 'lqksjhdkjsahdfkjsadhflkas ahjsdjkshdjkls';
+    })
+    console.log('end');
